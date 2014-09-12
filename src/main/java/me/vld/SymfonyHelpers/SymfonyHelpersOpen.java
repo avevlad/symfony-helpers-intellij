e72ba4c521564384aa9e3d013263853879451a98
AveVlad <dev@vld.me>
2014-09-12 19:57:39 +0400

    :+1: @alexander-doroshko

--- src/main/java/me/vld/SymfonyHelpers/SymfonyHelpersOpen.java
+++ src/main/java/me/vld/SymfonyHelpers/SymfonyHelpersOpen.java
@@ -1,5 +1,6 @@
 package me.vld.SymfonyHelpers;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -18,10 +19,6 @@ public class SymfonyHelpersOpen implements PsiScopeProcessor.Event {
      * @param route route
      */
     public static void open(String route, String type) {
-//        System.out.println(new java.util.Date());
-//        System.out.println(route);
-//        System.out.println(type);
-//        System.out.println(project);
 //        if (type.equals("controller")) {
 //            System.out.println("controller controller");
 //        }
@@ -29,40 +26,18 @@ public class SymfonyHelpersOpen implements PsiScopeProcessor.Event {
 //            System.out.println("view view");
 //        }
         VirtualFile file = baseDir.findFileByRelativePath("/rrr/1.txt");
-        System.out.println(file + " file");
-        System.out.println("-----------------");
         openFile(file);
     }
 
     public static void openFile(VirtualFile file) {
-        FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-        fileEditorManager.openFile(file, true);
-//        Application application = ApplicationManager.getApplication();
-//        application.runWriteAction(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("application.runWriteAction");
-//            }
-//        });
-    }
-
-    // тоже выдает исключение
-    public static class RunnableCreateAndOpenFile implements Runnable {
-
-        VirtualFile file;
-        Project project;
-
-        RunnableCreateAndOpenFile(Project project, VirtualFile file) {
-            this.project = project;
-            this.file = file;
-            run();
-        }
-
-        @Override
-        public void run() {
-            System.out.println("runrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
-            FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-            fileEditorManager.openFile(file, true, true);
-        }
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                if (file != null && file.isValid()) {
+                    FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+                    fileEditorManager.openFile(file, true);
+                }
+            }
+        });
     }
 }
